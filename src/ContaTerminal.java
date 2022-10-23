b862937400da8d76159f05c1687ce4ece64ca7d4
Daniel Teixeira <daniellhrt@hotmail.com>
2022-10-23 18:28:00 -0300

    Initial commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int conta = scan.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scan.next();
+
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String cliente = scan.next();
+
+        System.out.println("Por favor, digite o Saldo: ");
+        BigDecimal saldo = scan.nextBigDecimal();
+
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + conta + " e seu saldo R$:" + saldo +
+                ", já está disponível para saque.");
+
+        scan.close();
+
+
+
+    }
+}
